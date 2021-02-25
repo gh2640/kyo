796de80a43c4c16cf39dbdd7b563b8fea9f33595
Guilherme <guilherme.guedesguido@gmail.com>
2021-02-25 18:13:56 +0000

    Add balance command

--- src/kyo.rb
+++ src/kyo.rb
@@ -6,22 +6,24 @@ module KyoBot
   
   Bot = Discordrb::Commands::CommandBot.new token: ENV["TOKEN"], client_id: ENV["CLIENTID"], prefix: ENV["PREFIX"], ignore_bots: true
   
- # Db = Sequel.sqlite("database/database.db")
+  Db = Sequel.sqlite("database/database.db")
   
- # unless Db.table_exists(:users)
- #   Db.create_table :users do 
- #     Integer :id 
- #   end
- # end
+  unless Db.table_exists?(:users)
+    Db.create_table :users do 
+      Integer :id
+      Integer :money
+      Integer :bank
+    end
+  end
   
   Bot.ready do 
-  Bot.dnd 
-  Bot.game = "#{Bot.prefix}ping"
-  puts "O bot #{Bot.profile.name} está online"
+    Bot.dnd 
+    Bot.game = "#{Bot.prefix}ping"
+    puts "O bot #{Bot.profile.name} está online"
   end
  
   module Comandos; end
-  Dir["src/kyo/commands/*/*.rb"].each { |foda| load foda }
+  Dir["src/kyo/commands/*/*.rb"].each { |a| load a }
   Comandos.constants.each do |kkk|
     Bot.include! Comandos.const_get kkk
   end
