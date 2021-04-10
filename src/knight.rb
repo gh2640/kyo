1c447b67110afdc1a3805c65862e4b71ee9e3f06
gh2640 <guilherme.guedesguido@gmail.com>
2021-04-10 11:07:02 -0300

    Update knight.rb

--- src/knight.rb
+++ src/knight.rb
@@ -3,27 +3,38 @@ require "sequel"
 require "dotenv/load"
 
 module KnightBot
-  
-  Bot = Discordrb::Commands::CommandBot.new token: ENV["TOKEN"] || "token", client_id: ENV["CLIENTID"], prefix: ENV["PREFIX"], ignore_bots: true
+
+
+  Bot = Discordrb::Commands::CommandBot.new token: ENV["TOKEN"] || "token", client_id: ENV["CLIENTID"], prefix: "knight ", ignore_bots: true
   
   Db = Sequel.connect(ENV["POSTGRES"])
   
   unless Db.table_exists?(:users)
     Db.create_table :users do 
-      Integer :id
-      Boolean :blacklist
+      bigserial :id
+      boolean :blacklist
     end
   end
   
   Bot.ready do 
     Bot.dnd 
-    Bot.game = "#{Bot.prefix}help"
+    Bot.game = "teste"
     puts "O bot #{Bot.profile.name} está online"
   end
   
   Bot.message(start_with: Bot.prefix) do |event|
  
-  module Comandos; end
+ find_user_db = Db[:users].where(:id => event.user.id).first
+
+ if find_user_db == nil
+   Db[:users].insert(:id => event.user.id, :blacklist => false)
+   find_user_db = Db[:users].where(:id => event.user.id).first
+ end
+
+ break event.respond "Você está na blacklist" if find_user_db[:blacklist] == true
+ 
+ 
+ module Comandos; end
   Dir["src/modules/commands/*/*.rb"].each { |files| load files }
   Comandos.constants.each do |commands|
     Bot.include! Comandos.const_get commands
